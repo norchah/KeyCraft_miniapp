c3625b4e33e2a2aaa948c5b8e0211c7b1b3f0147
norchah <norchah@mail.ru>
2025-09-15 19:48:48 +0200

    add logo

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-[560px] min-h-[800px] bg-sky-800">
-      <div className="max-w-[485px] min-h-[600px] bg-sky-700">
-        <div className="max-w-[425px] min-h-[560px] bg-sky-600">
-          <p>First miniApp</p>
-        </div>
-      </div>
+    <div className="color-white p-20 max-w-[560px] min-h-[800px] bg-gray-800 flex flex-col items-center">
+      <Image src="/KeyCraft_logotype.png" alt="logo" width={80} height={80} />
+      <p>First miniApp</p>
     </div>
   );
 }
