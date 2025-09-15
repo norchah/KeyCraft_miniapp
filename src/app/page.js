786a61de912d9cba816a6e9c5401b7c222f1b27a
norchah <norchah@mail.ru>
2025-09-15 19:23:03 +0200

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-
+    <div className="outline max-w-[560px] min-h-[800px] bg-sky-800 flex justify-center items-center">
+       <p>First miniApp</p>
     </div>
   );
 }
