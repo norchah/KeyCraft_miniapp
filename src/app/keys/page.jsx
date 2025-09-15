30e6922c32e4d2ad2626da0606714c57fc39298f
norchah <norchah@mail.ru>
2025-09-16 01:14:12 +0200

    experiment the end

--- src/app/keys/page.jsx
+++ src/app/keys/page.jsx
@@ -1,48 +1,18 @@
 'use client';
 
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import Link from "next/link";
-import {UserApi} from "@/api/userApi";
+
 
 export default function KeysPage() {
   const [users, setUsers] = useState([]);
-
-
-  useEffect(() => {
-    // вызываем свой API route
-    fetch('/api/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-      .catch(err => console.error(err));
-  }, []);
-
-
-  // useEffect(() => {
-  //   async function getUsers() {
-  //     try {
-  //       const data = await api.get();
-  //       console.log("API data:", data);
-  //       setUsers(data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  //   getUsers();
-  // }, []); // пустой массив → вызов один раз при монтировании
 
 
   return (
     <div className='flex flex-col items-center'>
       <h3>Здесь будут твои ключи</h3>
       <Link href='/'
-            className='underline hover:text-gray-300 active:text-gray-400 transition-all duration-100'>Назад</Link>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <p>{user.tgId}</p>
-          </li>
-        ))}
-      </ul>
+       className='underline hover:text-gray-300 active:text-gray-400 transition-all duration-100'>Назад</Link>
     </div>
   );
 }
