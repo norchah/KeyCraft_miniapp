23652178f6fcb7a36068c09aafd346d1eda48d23
norchah <norchah@mail.ru>
2025-09-15 19:32:15 +0200

    test size of minmax window

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="outline max-w-[560px] min-h-[800px] bg-sky-800 flex justify-center items-center">
-       <p>First miniApp</p>
+    <div className="max-w-[560px] min-h-[800px] bg-sky-800">
+      <div className="max-w-[485px] min-h-[600px] bg-sky-700">
+        <div className="max-w-[425px] min-h-[560px] bg-sky-600">
+          <p>First miniApp</p>
+        </div>
+      </div>
     </div>
   );
 }
