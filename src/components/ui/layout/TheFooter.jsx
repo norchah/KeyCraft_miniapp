2c97c96516db36ff6d7115f944eb3e8de50e05da
norchah <norchah@mail.ru>
2025-09-18 00:46:10 +0200

    dev

--- src/components/ui/layout/TheFooter.jsx
+++ src/components/ui/layout/TheFooter.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function TheFooter() {
   return (
-    <nav className='fixed bottom-0 left-0 max-w-[420px] h-20 bg-gray-800'>sdfsdfsdf</nav>
+    <nav className='fixed bottom-0 left-0 max-w-[420px] min-w-[375px] h-20 bg-gray-800'>sdfsdfsdf</nav>
   );
 }
